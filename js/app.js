6930914fd04eab5739d06ce190c469f94a744d6b
Shin <shinnguyen93@gmail.com>
2021-10-23 16:40:48 +1030

    First Commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+
+  if (element) return element;
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  );
+};
+
+const date = getElement("#date");
+const currentYear = new Date().getFullYear();
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle('burgerToggle');
+  });
+};
+
+const app = () => {
+  document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+      document.querySelector("body").style.visibility = "hidden";
+      document.querySelector("#loader").style.visibility = "visible";
+    } else {
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector("body").style.visibility = "visible";
+    }
+  };
+
+  //Get current year
+  date.textContent = currentYear;
+
+  //Function for nav to slide in
+  navSlide();
+};
+
+app();
